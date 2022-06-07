d9c6eb6f42f26427aabcbc568185be90d5eba593
pengchihui1 <pengchihui1@gmail.com>
2022-06-07 15:25:58 +0800

    移动文件

--- lib/loading.dart
+++ lib/loading.dart
@@ -22,7 +22,7 @@ class _LoadingSate extends State<LoadingPage> {
   // 容器覆盖
   Widget build(BuildContext context) {
     return new Container(
-      child: Image.asset("images/loading.png",fit: BoxFit.cover,),
+      child: Image.asset("assets/images/loading.png",fit: BoxFit.cover,),
       // width: MediaQuery.of(context).size.width, // 屏幕宽度
       // height: MediaQuery.of(context).size.height, // 屏幕高度
       constraints: BoxConstraints.expand(), // 自适应屏幕
